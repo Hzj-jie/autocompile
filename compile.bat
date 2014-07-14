6e4aae4f20bb89cc6704ce9548d525c6163dbb0e
Hzj-jie <hzj_jie@hotmail.com>
2014-07-14 21:59:16 +0800

    optimize options for cl  Changes to be committed: 	modified:   compile.bat

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 
-cl /Wall /EHsc /c config.cpp /Foconfig.o
-cl /Wall /EHsc /c process_output.cpp /Foprocess_output.o
-cl /Wall /EHsc config_test.cpp /link config.o
-cl /Wall /EHsc process_output_test.cpp /link process_output.o
-cl /Wall /EHsc /Ic:\Boost\include\boost-1_55 autocompile.cpp /link config.o process_output.o
-cl /Wall /EHsc /Ic:\Boost\include\boost-1_55 maketree.cpp /link config.o process_output.o
+cl /Ox /Wall /EHsc /c config.cpp /Foconfig.o
+cl /Ox /Wall /EHsc /c process_output.cpp /Foprocess_output.o
+cl /Ox /Wall /EHsc config_test.cpp /link config.o
+cl /Ox /Wall /EHsc process_output_test.cpp /link process_output.o
+cl /Ox /Wall /EHsc /Ic:\Boost\include\boost-1_55 autocompile.cpp /link config.o process_output.o
+cl /Ox /Wall /EHsc /Ic:\Boost\include\boost-1_55 maketree.cpp /link config.o process_output.o
 del *.obj
 
