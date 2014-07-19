08bba7f26b5fb2621e6b82ef9b3bcba52db3f721
Hzj-jie <hzj_jie@hotmail.com>
2014-07-19 23:58:01 +0800

    output process_output error to cout

--- process_output.hpp
+++ process_output.hpp
@@ -68,10 +68,10 @@ static bool process_output(const std::string& s,
         if(!err.empty())
         {
             if(mtx != nullptr) mtx->lock();
-            cerr << "error detected when running " << s << endl;
+            cout << "error detected when running " << s << endl;
             for(size_t i = 0; i < err.size(); i++)
-                cerr << "l(" << i << ") " << err[i] << endl;
-            cerr << "usually this does not show a problem, just as a reference" << endl;
+                cout << "l(" << i << ") " << err[i] << endl;
+            cout << "usually this does not show a problem, just as a reference" << endl;
             if(mtx != nullptr) mtx->unlock();
         }
         return true;
